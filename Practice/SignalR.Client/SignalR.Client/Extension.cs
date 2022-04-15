ea1f8a1a391f049471f846a9b0bea0f81457c6cb
張家榮(裕隆) <016890@yulon-motor.com.tw>
2022-04-15 16:24:43 +0800

    新增Sqlite Reader 練習

---
+++ Practice/SignalR.Client/SignalR.Client/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace SignalR.Client
+{
+    static class Extension
+    {
+        public static void InvokeIfNecessary(this System.Windows.Forms.Control control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
