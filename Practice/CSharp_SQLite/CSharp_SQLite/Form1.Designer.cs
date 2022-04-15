3abbdbe6f6d76443d2022dc41a5407609b8983d9
張家榮(裕隆) <016890@yulon-motor.com.tw>
2022-04-15 16:17:23 +0800

    新增Sqlite Reader 練習

---
+++ Practice/CSharp_SQLite/CSharp_SQLite/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace CSharp_SQLite
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnConection = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.btnExeSql = new System.Windows.Forms.Button();
+			this.btnChooseFile = new System.Windows.Forms.Button();
+			this.txtFilePath = new System.Windows.Forms.TextBox();
+			this.txtSql = new System.Windows.Forms.TextBox();
+			this.dgvData = new System.Windows.Forms.DataGridView();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnConection
+			// 
+			this.btnConection.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnConection.Location = new System.Drawing.Point(501, 3);
+			this.btnConection.Name = "btnConection";
+			this.btnConection.Size = new System.Drawing.Size(94, 24);
+			this.btnConection.TabIndex = 0;
+			this.btnConection.Text = "Con";
+			this.btnConection.UseVisualStyleBackColor = true;
+			this.btnConection.Click += new System.EventHandler(this.btnConection_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.btnExeSql, 2, 1);
+			this.tableLayoutPanel1.Controls.Add(this.btnConection, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.btnChooseFile, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.txtFilePath, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.txtSql, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.dgvData, 0, 2);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(598, 372);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// btnExeSql
+			// 
+			this.btnExeSql.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnExeSql.Enabled = false;
+			this.btnExeSql.Location = new System.Drawing.Point(501, 33);
+			this.btnExeSql.Name = "btnExeSql";
+			this.btnExeSql.Size = new System.Drawing.Size(94, 24);
+			this.btnExeSql.TabIndex = 2;
+			this.btnExeSql.Text = "ExcuteSql";
+			this.btnExeSql.UseVisualStyleBackColor = true;
+			this.btnExeSql.Click += new System.EventHandler(this.btnExeSql_Click);
+			// 
+			// btnChooseFile
+			// 
+			this.btnChooseFile.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnChooseFile.Location = new System.Drawing.Point(401, 3);
+			this.btnChooseFile.Name = "btnChooseFile";
+			this.btnChooseFile.Size = new System.Drawing.Size(94, 24);
+			this.btnChooseFile.TabIndex = 3;
+			this.btnChooseFile.Text = "ChooseFile";
+			this.btnChooseFile.UseVisualStyleBackColor = true;
+			this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+			// 
+			// txtFilePath
+			// 
+			this.txtFilePath.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtFilePath.Enabled = false;
+			this.txtFilePath.Location = new System.Drawing.Point(3, 3);
+			this.txtFilePath.Name = "txtFilePath";
+			this.txtFilePath.Size = new System.Drawing.Size(392, 22);
+			this.txtFilePath.TabIndex = 4;
+			this.txtFilePath.Text = "data.db";
+			// 
+			// txtSql
+			// 
+			this.tableLayoutPanel1.SetColumnSpan(this.txtSql, 2);
+			this.txtSql.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtSql.Location = new System.Drawing.Point(3, 33);
+			this.txtSql.Name = "txtSql";
+			this.txtSql.Size = new System.Drawing.Size(492, 22);
+			this.txtSql.TabIndex = 5;
+			this.txtSql.Text = "SELECT * FROM DATA WHERE Date_Text BETWEEN \'2022/04/01\' AND \'2022/04/15\'";
+			// 
+			// dgvData
+			// 
+			this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.tableLayoutPanel1.SetColumnSpan(this.dgvData, 3);
+			this.dgvData.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dgvData.Location = new System.Drawing.Point(3, 63);
+			this.dgvData.Name = "dgvData";
+			this.dgvData.RowTemplate.Height = 24;
+			this.dgvData.Size = new System.Drawing.Size(592, 306);
+			this.dgvData.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(598, 372);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "SQLite_Reader";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnConection;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button btnExeSql;
+		private System.Windows.Forms.Button btnChooseFile;
+		private System.Windows.Forms.TextBox txtFilePath;
+		private System.Windows.Forms.TextBox txtSql;
+		private System.Windows.Forms.DataGridView dgvData;
+	}
+}
+
---
+++ Practice/CSharp_SQLite/CSharp_SQLite/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_SQLite
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();			
+			//// 建立資料表 TestTable
+			//var createtablestring = @"CREATE TABLE TestTable (Foo double, Bar double);";
+			//CreateTable("data.db", createtablestring);
+
+			//// 插入資料到 TestTable 表中
+			//var insertstring = @"
+			//	INSERT INTO TestTable (Foo, Bar) VALUES ('10', '100');
+			//	INSERT INTO TestTable (Foo, Bar) VALUES ('20', '200');
+			//";
+			//Manipulate("data.db", insertstring);	
+		}
+
+		/// <summary>建立資料庫連線</summary>
+		/// <param name="database">資料庫名稱</param>
+		/// <returns></returns>
+		public SQLiteConnection OpenConnection(string database)
+		{
+			var conntion = new SQLiteConnection()
+			{
+				ConnectionString = $"Data Source={database};Version=3;New=False;Compress=True;"
+			};
+			if (conntion.State == ConnectionState.Open) conntion.Close();
+			conntion.Open();
+			return conntion;
+		}
+
+		/// <summary>建立新資料庫</summary>
+		/// <param name="database">資料庫名稱</param>
+		public void CreateDatabase(string database)
+		{
+			string path = @"d:\test\"+ database;
+			SQLiteConnection cn = new SQLiteConnection("data source=" + path+ ";Version=3;New=True;Compress=True;");
+			cn.Open();
+			cn.Close();
+		}
+
+		/// <summary>建立新資料表</summary>
+		/// <param name="database">資料庫名稱</param>
+		/// <param name="sqlCreateTable">建立資料表的 SQL 語句</param>
+		public void CreateTable(string database, string sqlCreateTable)
+		{
+			var connection = OpenConnection(database);
+			//connection.Open();
+			var command = new SQLiteCommand(sqlCreateTable, connection);
+			var mySqlTransaction = connection.BeginTransaction();
+			try
+			{
+				command.Transaction = mySqlTransaction;
+				command.ExecuteNonQuery();
+				mySqlTransaction.Commit();
+			}
+			catch (Exception ex)
+			{
+				mySqlTransaction.Rollback();
+				throw (ex);
+			}
+			if (connection.State == ConnectionState.Open) connection.Close();
+		}
+
+		/// <summary>新增\修改\刪除資料</summary>
+		/// <param name="database">資料庫名稱</param>
+		/// <param name="sqlManipulate">資料操作的 SQL 語句</param>
+		public void Manipulate(string database, string sqlManipulate)
+		{
+			var connection = OpenConnection(database);
+			var command = new SQLiteCommand(sqlManipulate, connection);
+			var mySqlTransaction = connection.BeginTransaction();
+			try
+			{
+				command.Transaction = mySqlTransaction;
+				command.ExecuteNonQuery();
+				mySqlTransaction.Commit();
+			}
+			catch (Exception ex)
+			{
+				mySqlTransaction.Rollback();
+				throw (ex);
+			}
+			if (connection.State == ConnectionState.Open) connection.Close();
+		}
+
+		/// <summary>讀取資料</summary>
+		/// <param name="database">資料庫名稱</param>
+		/// <param name="sqlQuery">資料查詢的 SQL 語句</param>
+		/// <returns></returns>
+		public DataTable GetDataTable(string database, string sqlQuery)
+		{
+			try
+			{
+				var connection = OpenConnection(database);
+				var dataAdapter = new SQLiteDataAdapter(sqlQuery, connection);
+				var myDataTable = new DataTable();
+				var myDataSet = new DataSet();
+				myDataSet.Clear();
+				dataAdapter.Fill(myDataSet);
+				myDataTable = myDataSet.Tables[0];
+				if (connection.State == ConnectionState.Open) connection.Close();
+				return myDataTable;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Excute SQL Command Error {0}",ex.Message);
+				return null;
+			}
+		}
+
+		private void btnConection_Click(object sender, EventArgs e)
+		{
+			// 建立 SQLite 資料庫 連線
+			if (OpenConnection(txtFilePath.Text).State == ConnectionState.Open)
+			{
+				MessageBox.Show("Connection Success");
+				btnExeSql.Enabled = true;
+			}
+			else
+			{
+				MessageBox.Show("Connection Faild");
+				btnExeSql.Enabled = false;
+			}
+		}
+
+		private void btnExeSql_Click(object sender, EventArgs e)
+		{
+			// 讀取資料
+			var dataTable = GetDataTable(txtFilePath.Text, txtSql.Text);
+			dgvData.DataSource = dataTable;
+		}
+
+		private void btnChooseFile_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Multiselect = false;//該值確定是否可以選擇多個檔案
+			dialog.Title = "請選擇資料庫";
+			dialog.Filter = "所有檔案(*.*)|*.*";
+			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				txtFilePath.Text = dialog.FileName;
+			}
+		}
+	}
+}
